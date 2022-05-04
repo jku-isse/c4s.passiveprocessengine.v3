b3cb5b0274aa69f6ce9b5c60de405c09fe89d470
Christoph Mayr-Dorn <c.mayr-dorn@jku.at>
2022-05-04 19:54:11 +0200

    added derivedproperty test

--- src/test/java/at/jku/isse/designspace/passiveprocessengine/instance/RepairTests.java
+++ src/test/java/at/jku/isse/designspace/passiveprocessengine/instance/RepairTests.java
@@ -1,5 +1,6 @@
 package at.jku.isse.designspace.passiveprocessengine.instance;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -20,6 +21,7 @@ import at.jku.isse.designspace.rule.arl.repair.RepairNode;
 import at.jku.isse.designspace.rule.checker.ArlRuleEvaluator;
 import at.jku.isse.designspace.rule.checker.ConsistencyUtils;
 import at.jku.isse.designspace.rule.model.ConsistencyRuleType;
+import at.jku.isse.designspace.rule.model.DerivedPropertyRuleType;
 import at.jku.isse.designspace.rule.model.Rule;
 import at.jku.isse.designspace.rule.service.RuleService;
 import at.jku.isse.passiveprocessengine.WrapperCache;
@@ -74,5 +76,54 @@ class RepairTests {
 		;
 	}
 	
+	@Test
+	void testReevaluationOnReferenceChange() {
+		Instance jiraB =  TestArtifacts.getJiraInstance(ws, "jiraB");
+		Instance jiraC = TestArtifacts.getJiraInstance(ws, "jiraC");
+		Instance jiraD = TestArtifacts.getJiraInstance(ws, "jiraD");
+		Instance jiraA = TestArtifacts.getJiraInstance(ws, "jiraA");
+		TestArtifacts.addParentToJira(jiraA, jiraB); //link from A to B (as parent
+		TestArtifacts.addParentToJira(jiraB, jiraC);
+		TestArtifacts.addParentToJira(jiraC, jiraD); 
+		
+		ConsistencyRuleType crt = ConsistencyRuleType.create(ws, typeJira, "TranverseTest", "self.parent.parent.name.equalsIgnoreCase('jiraD')");
+		ws.concludeTransaction();
+		crt.consistencyRuleEvaluations().value.forEach(cr -> { RepairNode repairTree = RuleService.repairTree(cr); });
+		;
+		System.out.println("UPDATING PARENT");
+		TestArtifacts.addParentToJira(jiraB, jiraD);
+		ws.concludeTransaction();
+		crt.consistencyRuleEvaluations().value.forEach(cr -> { RepairNode repairTree = RuleService.repairTree(cr); });
+	}
+	
+	@Test
+	void testDerivedPropertyFromDerivedProperty() {
+		
+		InstanceType instanceType = WorkspaceService.createInstanceType(ws, "TaskType", ws.TYPES_FOLDER);
+        WorkspaceService.createPropertyType(ws, instanceType, "in", Cardinality.SINGLE, typeJira);
+        WorkspaceService.createPropertyType(ws, instanceType, "out", Cardinality.SINGLE, typeJira);
+        WorkspaceService.createPropertyType(ws, instanceType, "prev", Cardinality.SINGLE, instanceType);
+        
+        DerivedPropertyRuleType dPropIn2Out = DerivedPropertyRuleType.create(ws, instanceType, "out", Cardinality.SINGLE, "if self.in.isDefined() and self.in.parent.isDefined() then self.in.parent else null endif");
+        DerivedPropertyRuleType dPropout2In = DerivedPropertyRuleType.create(ws, instanceType, "in", Cardinality.SINGLE, "if self.prev.isDefined() and self.prev.out.isDefined() then self.prev.out else null endif");
+
+                
+        Instance jiraB =  TestArtifacts.getJiraInstance(ws, "jiraB");
+		Instance jiraC = TestArtifacts.getJiraInstance(ws, "jiraC");
+		TestArtifacts.addParentToJira(jiraB, jiraC);
+        Instance step1 = WorkspaceService.createInstance(ws, "step1", instanceType);
+        Instance step2 = WorkspaceService.createInstance(ws, "step1", instanceType);
+        step2.getProperty("prev").set(step1);
+
+        ws.concludeTransaction();
+        assertEquals(null, step1.getPropertyAsValue("out"));
+        step1.getProperty("in").set(jiraB);
+       
+        ws.concludeTransaction();
+        assertEquals(jiraC, step1.getPropertyAsValue("out"));
+        assertEquals(jiraC, step2.getPropertyAsValue("in"));
+        
+		
+	}
 	
 }
